528ddc8e8dcadae498a06d022b9e3e2ed32a15ef
Kevin Solis <soliskayvon@gmail.com>
2023-10-19 16:54:22 -0700

    upload of index and asset files

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+// Css script code//
+
+var colors = new Array(
+    [62, 35, 255],
+    [60, 255, 60],
+    [255, 35, 98],
+    [45, 175, 230],
+    [255, 0, 255],
+    [255, 128, 0]);
+
+var step = 0;
+var colorIndices = [0, 1, 2, 3];
+var gradientSpeed = 0.002;
+function updateGradient() {
+
+    if ($ === undefined) return;
+
+    var c0_0 = colors[colorIndices[0]];
+    var c0_1 = colors[colorIndices[1]];
+    var c1_0 = colors[colorIndices[2]];
+    var c1_1 = colors[colorIndices[3]];
+
+    var istep = 1 - step;
+    var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+    var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+    var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+    var color1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
+
+    var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+    var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+    var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+    var color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
+
+    $('#gradient').css({
+        background: "-webkit-gradient(linear, left top, right top, from(" + color1 + "), to(" + color2 + "))"
+    }).css({
+        background: "-moz-linear-gradient(left, " + color1 + " 0%, " + color2 + " 100%)"
+    });
+    step += gradientSpeed;
+    if (step >= 1) {
+        step %= 1;
+        colorIndices[0] = colorIndices[1];
+        colorIndices[2] = colorIndices[3];
+        colorIndices[1] = (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+        colorIndices[3] = (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+
+    }
+}
+
+setInterval(updateGradient, 10);
+
+// code for quiz interactivity//
+
+function submitAnswers () {
+    var total = 5;
+    var score = 0;
+
+    //get user input//
+    var q1 = document.forms["quizForm"]["q1"].value;
+    var q2 = document.forms["quizForm"]["q2"].value;
+    var q3 = document.forms["quizForm"]["q3"].value;
+    var q4 = document.forms["quizForm"]["q4"].value;
+    var q5 = document.forms["quizForm"]["q5"].value;
+
+    for (i = 1; i <= total; i++) {
+        if (eval('q'+i) == null || eval('q'+i) == '') {
+            alert('You missed question '+i );
+            return false;
+        }
+    }
+
+// correct answers //
+var answers = ["All the above","Alerts","Curly brackets","Quotes","Add interactivity to webpages"];
+
+// check answers //
+for (i = 1; i <= total; i++) {
+    if(eval('q'+i) == answers[i - 1]) {
+        score++;
+    }
+    }
+
+    //display total score//
+    var results = document.getElementById('results');
+    results.innerHTML = '<h3>You scored <span>'+score+'</span> out of <span>'+total+'</span></h3>';
+    alert('You scored '+score+'out of ' +total)
+
+// to avoid empty or incorrect responses//
+return false;
+}
